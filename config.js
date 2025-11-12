24f55a36071982cf34d9cbcd920fa4d4980ae809
LordSaturn01 <leilabloody@gmail.com>
2025-11-12 17:28:30 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+export default {
+  BotName: "solars-md", //Bot name 
+  nameCreator: "SaturnDev", // your name 
+  Newsletter: "120363401981326696@newsletter",//newsletter for your channel
+  Newsletter2: " newsletter from your channel ",//optimel
+  Newsletter3: " Newsletter for your channel", //optimel
+
+Newsletter4: "Newsletter for your channel", //optimel
+
+Newsletter5: "Newsletter for your channel", //optimel
+
+Newsletter6: "Newsletter for your channel",//optimel 
+};
